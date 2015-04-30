f00586f42efbe7b9d098c88c1c65c11d0a4ceede
Brendan HomePC <brenoates@gmail.com>
2015-04-30 18:42:51 +0100

    persona now working

--- FatBurn/validators.py
+++ FatBurn/validators.py
@@ -6,7 +6,7 @@ def validate_weight(value):
         raise ValidationError('{} is not within the valid weight range'.format(value))
 
 def validate_height(value):
-    if not (is_number(value) and value > 0.5 and value < 2.5):
+    if not (is_number(value) and value > 0.5 and value < 3.0):
         raise ValidationError('{} is not within the valid height range'.format(value))
 
 def is_number(s):
